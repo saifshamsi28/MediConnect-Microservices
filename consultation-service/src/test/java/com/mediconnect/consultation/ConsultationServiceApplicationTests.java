a7bcffa9ad9273f4a261443ecddc4d75be914f85
Toxic-saif <saifshamsi9905@gmail.com>
2026-02-03 04:04:44 +0530

    Remove secrets from .env

---
+++ consultation-service/src/test/java/com/mediconnect/consultation/ConsultationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediconnect.consultation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
