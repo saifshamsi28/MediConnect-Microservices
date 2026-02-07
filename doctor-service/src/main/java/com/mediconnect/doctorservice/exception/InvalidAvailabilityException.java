67012129e834e0af3a173375e8b868ffe56592fb
Toxic-saif <saifshamsi9905@gmail.com>
2026-02-07 16:53:42 +0530

    Created custom error and add to GlobalExceptionHandler

---
+++ doctor-service/src/main/java/com/mediconnect/doctorservice/exception/InvalidAvailabilityException.java
@@ -0,0 +1,7 @@
+package com.mediconnect.doctorservice.exception;
+
+public class InvalidAvailabilityException extends RuntimeException {
+    public InvalidAvailabilityException(String message) {
+        super(message);
+    }
+}
