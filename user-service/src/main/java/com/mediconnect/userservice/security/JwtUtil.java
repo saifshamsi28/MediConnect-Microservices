46402e90a8e69c2d58adfcabe122540ba308ac99
amir-khusroo <amirkhusroo250@gmail.com>
2026-02-07 18:37:07 +0530

    modified these file according to login requirement

--- user-service/src/main/java/com/mediconnect/userservice/security/JwtUtil.java
+++ user-service/src/main/java/com/mediconnect/userservice/security/JwtUtil.java
@@ -2,6 +2,7 @@ package com.mediconnect.userservice.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
@@ -10,15 +11,15 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private final String jwtSecretKey="jhdiuqjquu8wgwu8yje8ol2msyjsokdnbeguwkmbh";
+    private static final String jwtSecretKey="jhdiuqjquu8wgwu8yje8ol2msyjsokdnbeguwkmbh";
     private static final long EXPIRATION_TIME = 1000 * 60 * 60;
 
-    public String generateToken(String username, String role){
+    public String generateToken(UserDetails userDetails){
         return Jwts.builder()
-                .setSubject(username)
-                .claim("role",role)
+                .setSubject(userDetails.getUsername())
+                .claim("role",userDetails.getAuthorities().iterator().next().getAuthority())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis()+EXPIRATION_TIME))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60))
                 .signWith(Keys.hmacShaKeyFor(jwtSecretKey.getBytes(StandardCharsets.UTF_8)))
                 .compact();
     }
--- user-service/src/main/java/com/mediconnect/userservice/service/UserService.java
+++ user-service/src/main/java/com/mediconnect/userservice/service/UserService.java
@@ -3,5 +3,5 @@ package com.mediconnect.userservice.service;
 import com.mediconnect.userservice.requestDto.UserRegisterRequest;
 
 public interface UserService {
-    void registerUser(UserRegisterRequest request);
+
 }
