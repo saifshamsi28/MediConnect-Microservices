675ba0516ece36957c60a468de2666cac92d3aab
Md-sarfraz <mdsarfraz62070@gmail.com>
2026-02-10 19:54:20 +0530

    created necessary entities and implemented appointment booking feature

---
+++ appointment-service/src/main/java/com/mediconnect/appointmentservice/repository/AppointmentRepository.java
@@ -0,0 +1,23 @@
+package com.mediconnect.appointmentservice.repository;
+
+import com.mediconnect.appointmentservice.entity.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, UUID> {
+
+    boolean existsByDoctorIdAndSlotStart(UUID doctorId, LocalDateTime slotStart);
+
+
+    List<Appointment> findByDoctorId(UUID doctorId);
+
+//    List<Appointment> findByPatientId(UUID patientId);
+    List<Appointment> findByDoctorIdAndSlotStartBetween(
+            UUID doctorId,
+            LocalDateTime start,
+            LocalDateTime end
+    );
+}
